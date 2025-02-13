f6f2f933d44e2c8638df65043862f3264801c6b4
ismailmohammed1432 <survivorawais@gmail.com>
2025-02-13 11:35:35 +0530

    Portfolio

---
+++ server/app.js
@@ -0,0 +1,31 @@
+import express from "express"
+import path from "path";
+import {fileURLToPath} from "url";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+const PORT = 5005;
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("/rahman", (req, res)=>{
+    try{
+        res.status(200).json({msg: "hello rahman"})
+    } catch(error){
+        res.status(401).json(error)
+    }
+ });
+ 
+
+ app.get("/*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+ });
+
+ app.listen(PORT, ()=>{
+    console.log('YOUR SERVER IS UP AND RUNNING!');
+    
+ })
